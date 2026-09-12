0751233e5abb93ce585460a71667aaa3f85a2fbd
mahmadkaramshah <m.ahmadkaramshah@gmail.com>
2026-09-12 23:42:33 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
+  return {base: '/darul-uloom-ghousia-sargodha/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
